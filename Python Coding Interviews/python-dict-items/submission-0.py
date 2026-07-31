c24b338d83f8ed49eff2b949f57b135e17ac5116
Krystian <147599025+krystian-to@users.noreply.github.com>
2026-07-31 14:37:34 +0200

    Add: python-dict-items - submission-0

---
+++ Python Coding Interviews/python-dict-items/submission-0.py
@@ -0,0 +1,19 @@
+from typing import Dict, List, Tuple
+
+
+def get_dict_items(age_dict: Dict[str, int]) -> List[Tuple[str, int]]:
+    tuple_map = []
+
+    for key, value in age_dict.items():
+        pair = (key, value)
+        tuple_map.append(pair)
+
+    return tuple_map
+
+
+
+# do not modify below this line
+print(get_dict_items({'Alice': 25, 'Bob': 30, 'Charlie': 35}))
+print(get_dict_items({'Alice': 25, 'Bob': 30, 'Charlie': 35, 'David': 40}))
+print(get_dict_items({'Bob': 30, 'David': 40, 'Charlie': 35, 'Alice': 25, 'Eve': 45}))
+print(get_dict_items({'Alice': 25, 'Bob': 30, 'Charlie': 35, 'David': 40, 'Eve': 45, 'Frank': 50}))
